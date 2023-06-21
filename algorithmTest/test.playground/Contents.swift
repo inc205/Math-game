2f8e6aa51d092c61c42e2f3ce66e8c011bb7f40e
Diego Cando <diegoramoncito@gmail.com>
2023-06-20 20:23:58 -0500

    Simplified the Question object and fixed issues

--- algorithmTest/test.playground/Contents.swift
+++ algorithmTest/test.playground/Contents.swift
@@ -2,29 +2,26 @@ import UIKit
 
 
 struct Question {
-    var num1:Int = 0
-    var num2:Int? = 0
-    var operation:String = "+"
+    var question:String = ""
     var checkExisting:String = ""
-    var result:Int? = 0
     var correct:Bool = false
     var options:[Responses]
 }
 
 struct Responses {
-    var result:Int=0
+    var result:Int = 0
     var correct:Bool = false
+    var selected:Bool = false
 }
 
 //Test variables
 var listNumber1 = [2,3,5]
-var listNumber2 = [1,2,3,4,5,6,7,8,9]
-var listOperations = ["+","-","x"]
+var listNumber2 = [1,2,3,4,5,6,7,8,9,10,11]
+var listOperations = ["+","-","x","÷"]
 var numberQuestions = 15
 
 
 func checkExisting(check new:String, inside questions:[Question])-> Bool {
-    
     return questions.contains{ c in
         return c.checkExisting == new
     }
@@ -34,37 +31,63 @@ func buildQuestions(a:[Int], b:[Int], operations:[String], count:Int)->[Question
     
     var questions = [Question]()
     
-    for _ in 0..<count {
+    repeat {
         
         var na = a.randomElement()!
         var nb = b.randomElement()!
         var o = operations.randomElement()!
+        var question = ""
+        var existing = ""
         var re = 0
         switch o {
             case "+":
                 re = na + nb
+                question = "\(na) \(o) \(nb)= __"
+                existing = "\(na)\(o)\(nb)=\(re)"
                 break
             case "-":
-                re = na - nb
+                re = na + nb
+                question = "\(re) \(o) \(na) = __"
+                existing = "\(re)\(o)\(na)=\(nb)"
                 break
             case "x":
                 re = na * nb
+                question = "\(na) \(o) \(nb) = __"
+                existing = "\(na)\(o)\(nb)=\(re)"
                 break
-        default:
-            re = 0
+            case "÷":
+                re = na * nb
+                question = "\(re) \(o) \(na) = __"
+                existing = "\(re)\(o)\(na)=\(nb)"
+                break
+            default:
+                re = 0
         }
         var op = [Responses]()
         op.append(Responses(result: re,correct: true))
-        for j in 0..<3 {
-            op.append(Responses(result: re+j, correct: false))
+        for j in 1..<4 {
+            if([1,2].randomElement() == 1){
+                op.append(Responses(result: re+j, correct: false))
+            }else{
+                op.append(Responses(result: re-j, correct: false))
+            }
         }
-        print("\(na)\(o)\(nb)=\(re)")
-        print(checkExisting(check: "\(na)\(o)\(nb)=\(re)", inside: questions))
-        questions.append(Question(num1: na, num2: nb, operation: o, checkExisting: "\(na)\(o)\(nb)=\(re)", correct: false, options: op.shuffled()))
-    }
-    
+        //print("\(question) \(existing)")
+        //print(checkExisting(check: existing, inside: questions))
+        if(!checkExisting(check: existing, inside: questions)){
+            questions.append(Question(question: question, checkExisting: existing, correct: false, options: op.shuffled()))
+        }
+    } while questions.count < count
+    //print(questions.count)
     return questions.shuffled()
 }
 
 
-print(buildQuestions(a: listNumber1, b: listNumber2, operations: listOperations, count: numberQuestions))
+var temp = buildQuestions(a: listNumber1, b: listNumber2, operations: listOperations, count: numberQuestions)
+
+for el in temp {
+    print(el.question)
+    for pr in el.options {
+        print("\(pr.result) -> \(pr.correct)")
+    }
+}
