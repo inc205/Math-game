0d49d8ad0e5e6d0d4e513c1a2d71d2b473ae5acc
Diego Cando <diegoramoncito@gmail.com>
2023-07-04 16:09:38 -0500

    Refined algorithm

--- androidApp/Trivi/app/src/main/java/com/inc205/trivi/ui/trivia/model/QuestionModel.kt
+++ androidApp/Trivi/app/src/main/java/com/inc205/trivi/ui/trivia/model/QuestionModel.kt
@@ -1,2 +1,14 @@
 package com.inc205.trivi.ui.trivia.model
 
+data class Question(
+    var question:String,
+    var checkExisting:String,
+    var correct:Boolean,
+    var options:List<Responses>
+)
+
+data class Responses(
+    var result:Int,
+    var correct:Boolean,
+    var selected:Boolean
+)
